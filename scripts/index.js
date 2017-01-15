b9d59d71e49df92091e5914b9d28ab9f5df9b611
Femi Olukoya <oolukoya@andrew.cmu.edu>
2017-01-14 20:45:35 -0500

    Made slider vertical

--- scripts/index.js
+++ scripts/index.js
@@ -11,16 +11,17 @@ $(function() {
         min: 0,
         max: 100,
         step: .1,
+        orientation: "vertical",
         slide: function(event, ui) {
             tl.pause();
             //adjust the timeline’s progress() based on slider value
-            tl.progress(ui.value / 100);
+            tl.progress(1 - ui.value / 100);
         }
     });
 
     // updateSlider function
     function updateSlider() {
-        $("#slider").slider("value", tl.progress() * 100);
+        $("#slider").slider("value", 100 - tl.progress() * 100);
     }
 
     tl.staggerFrom(timelineBoxes, 1, {x: '-=100', autoAlpha: 0, ease:Power4.easeInOut}, 0.5);
