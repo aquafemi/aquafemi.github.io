57eaecfadcb8c14969fd0bd16440657c31b47b7e
Femi <oolukoya96@gmail.com>
2024-06-22 21:58:38 +0000

    Windows XP theme

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,10 +1,18 @@
 var sr = ScrollReveal({
-	origin   : "bottom",
-	distance : "64px",
-	duration : 800,
-	delay    : 0,
-	scale    : 1
+	origin: "bottom",
+	distance: "64px",
+	duration: 800,
+	delay: 0,
+	scale: 1
 });
 
 sr.reveal('.projects-list a');
 sr.reveal('.posts-list a');
+
+document.getElementById('.open-dialog').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close-dialog').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
