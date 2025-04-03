ef84162a3a7f17f9ec35b183332340f9b08e5a18
Femi Olukoya <oolukoya96@gmail.com>
2025-04-02 22:14:29 -0700

    start getting rid of projects

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,20 +1,11 @@
-var sr = ScrollReveal({
-	origin: "bottom",
-	distance: "64px",
-	duration: 800,
-	delay: 0,
-	scale: 1
+
+
+document.querySelector(".maximize-dialog").addEventListener("click", function () {
+	// open the dialog
+	console.log("Dialog maximized");
 });
 
-sr.reveal('.projects-list a');
-sr.reveal('.posts-list a');
-
-document.querySelector('open-dialog').addEventListener("click", function() {
-	document.querySelector('.window.bg-modal').style.display = "flex";
-	console.log("opening")
-});
-
-document.querySelector('.close-dialog').addEventListener("click", function() {
-	document.querySelector('.window.bg-modal').style.display = "none";
-	console.log("closing")
+document.querySelector(".minimize-dialog").addEventListener("click", function () {
+	// open the dialog
+	console.log("Dialog minimized");
 });
